a291b3ffd5acf500031895019899e876e1a6ac88
MisterCore <Heiko-YouTube@web.de>
2026-01-17 19:07:28 +0100

    Bump default version to 1.0.1 in `build.gradle.kts`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 val versions = providers.gradleProperty("net.labymod.minecraft-versions").get().split(";")
 
 group = "de.evilradio"
-version = providers.environmentVariable("VERSION").getOrElse("1.0.0")
+version = providers.environmentVariable("VERSION").getOrElse("1.0.1")
 
 labyMod {
     defaultPackageName = "de.evilradio" //change this to your main package name (used by all modules)
