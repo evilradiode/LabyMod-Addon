0de1a52a222b182453be360e3a572f753b63052b
MisterCore <Heiko-YouTube@web.de>
2026-09-08 21:10:18 +0200

    Removed unused `ReleaseChannels` import and `releaseChannel` property from `build.gradle.kts`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import net.labymod.labygradle.common.extension.model.labymod.ReleaseChannels
-
 plugins {
     id("net.labymod.labygradle")
     id("net.labymod.labygradle.addon")
@@ -31,7 +29,6 @@ labyMod {
         description = "Ein Radio-Addon für LabyMod 4"
         minecraftVersion = "*"
         version = rootProject.version.toString()
-        releaseChannel = ReleaseChannels.SNAPSHOT
     }
 }
 
